fc5f3847c73767ac53fdf4ec8bceb8ee882de418
Eslam Salem <netcodez@gmail.com>
2017-08-01 12:10:48 +0200

    merge

--- src/ShieldfyServiceProvider.php
+++ src/ShieldfyServiceProvider.php
@@ -3,19 +3,37 @@
 namespace Shieldfy\ShieldfyLaravelClient;
 
 use Illuminate\Support\ServiceProvider;
+use Shieldfy\ShieldfyLaravelClient\Middlewares\ShieldfyMiddleware;
 
 class ShieldfyServiceProvider extends ServiceProvider
 {
     /**
      * Bootstrap the application services.
+     * 
+     * @param \Illuminate\Routing\Router $router
      *
      * @return void
      */
-    public function boot()
+    public function boot(Router $router)
     {
         $this->publishes([
             __DIR__.'/config/shieldfy.php' => config_path('shieldfy.php'),
-        ]);
+        ], 'config');
+        //register middlewares
+        $this->registerMiddleWare($router);
+    }
+
+    /**
+     * register middleware.
+     *
+     * @param \Illuminate\Routing\Router $router
+     *
+     * @return void
+     */
+    protected function registerMiddleWare(Router $router)
+    {
+        // Append middleware to the 'web' middlware group
+        $router->pushMiddlewareToGroup('web', ShieldfyMiddleware::class);
     }
 
     /**
@@ -25,5 +43,7 @@ class ShieldfyServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        // Merge config
+        $this->mergeConfigFrom( __DIR__.'/config/shieldfy.php', 'shieldfy');
     }
 }
